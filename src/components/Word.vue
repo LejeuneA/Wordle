1d0dd111309c971187d6c3287878694535081443
Sh1r0yuki <thomas.leo@live.be>
2025-05-13 10:55:53 +0200

    new file:   src/components/Word.vue

---
+++ src/components/Word.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="word">
+    <Letter
+      v-for="(char, index) in letters"
+      :key="index"
+      :char="char"
+      :status="statuses[index]"
+    />
+  </div>
+</template>
+
+<script setup>
+import Letter from './Letter.vue'
+defineProps({
+  letters: Array,
+  statuses: Array
+})
+</script>
+
+<style scoped>
+.word {
+  display: flex;
+  gap: 0.5rem;
+}
+</style>
